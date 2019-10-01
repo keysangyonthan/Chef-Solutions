a9cff5bf708ec172db7e4764506d2628fcb0313f
Keysang Yonthan <keysangkeysang@gmail.com>
2019-10-01 23:00:32 +0530

    program to mirror a number

---
+++ mnumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int a, b, ans;
+        cin>>a>>b;
+        
+        ans = a+b;
+        
+        cout<<ans<<endl;
+    }
+    return 0;
+}
